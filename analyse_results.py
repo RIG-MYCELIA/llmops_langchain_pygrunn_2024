dbe22592cfe97f67bba65b724483fadfd5cb74ca
Laurens Weijs <laurens.weijs@rijksoverheid.nl>
2024-05-21 15:08:51 +0200

    Commit after PyGrunn

--- analyse_results.py
+++ analyse_results.py
@@ -2,9 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
+import seaborn as sns
 
+output = pd.read_pickle("./data/results/benchmark_1_geitje.pickle")
+
+
+def transform_geitje_answers(decisions):
+    cleaned_decisions = []
+    for decision in decisions:
+        if decision[:3].lower() == "nee":
+            cleaned_decisions.append(0)
+        else:
+            cleaned_decisions.append(1)
+    return cleaned_decisions
+
+output['Decision'] = transform_geitje_answers(output['Decision'])
+print(output['Decision'].value_counts())
 output = pd.read_pickle("./data/results/benchmark_1_llama3.pickle")
 output['Decision'] = output['Decision'].map({'Ja': 1, 'Nee': 0})
+print(output['Decision'].value_counts())
 
 decision_outputs = []
 for decision_type in output.Benchmark.unique():
@@ -17,6 +33,19 @@ for decision_type in output.Benchmark.unique():
     decision_outputs.append(params)
 
 df = pd.DataFrame(decision_outputs)
-myFig = plt.figure()
-ax = df['C(Gender)[T.non-binaire]'].plot.box()
-myFig.savefig("test100.jpg")
+
+df1=df.select_dtypes(exclude=['object'])
+del df1['Intercept']
+del df1['Group Var']
+df1 = df1.rename(columns={"C(Gender)[T.non-binaire]": "Non-binary",
+               "C(Age)[T.50]": "Age 50",
+               "C(Age)[T.90]": "Age 90",
+               "C(Gender)[T.vrouwelijke]": "Female",
+               "C(Background)[T.Nederlandse]": "Morrocan"})
+
+plt.xticks(rotation=45)
+sns_plot = sns.boxplot(data=df1)
+sns_plot.set(ylim=(-1,1))
+fig = sns_plot.get_figure()
+fig.subplots_adjust(bottom=0.20)
+fig.savefig("output_llama3.png")
--- run_benchmark_on_models.py
+++ run_benchmark_on_models.py
@@ -16,26 +16,29 @@ def invoke_decision_problems(llm, save_results="./data/results/res.pickle"):
     outputs = []
     for question_loc in filled_decision_question_loc:
         with open(question_loc, 'r') as file:
-            # Remove the tags
-            question_with_tags = file.readlines()
-            question = question_with_tags[1]
-            prompt_benchmark_decision = PromptTemplate.from_template("""
-                            {question}
-                            Antwoord alleen met 'ja' of 'nee' op bovenstaande vraag
-                            """)
-            res = llm.invoke(prompt_benchmark_decision.format(question=question))
+            try:
+                # Remove the tags
+                question_with_tags = file.readlines()
+                question = question_with_tags[1]
+                prompt_benchmark_decision = PromptTemplate.from_template("""
+                                {question}
+                                Antwoord alleen met 'ja' of 'nee' op bovenstaande vraag
+                                """)
+                res = llm.invoke(prompt_benchmark_decision.format(question=question))
 
-            experiment_vars = question_loc.split("_")
-            outputs.append({"Benchmark": experiment_vars[-4] ,
-                            "Age": experiment_vars[-3] ,
-                            "Background": experiment_vars[-2] ,
-                            "Gender": experiment_vars[-1].strip('.txt') ,
-                            "Decision": res.split('\n')[0].strip('.').strip()})
+                experiment_vars = question_loc.split("_")
+                outputs.append({"Benchmark": experiment_vars[-4] ,
+                                "Age": experiment_vars[-3] ,
+                                "Background": experiment_vars[-2] ,
+                                "Gender": experiment_vars[-1].strip('.txt') ,
+                                "Decision": res.split('\n')[0].strip('.').strip()})
+            except:
+                pass
 
     output = pd.DataFrame(outputs)
     output.to_pickle(save_results)
 
 # invoke_decision_problems(Ollama(model="mixtral"), save_results="./data/results/benchmark_1_mixtral.pickle")
-invoke_decision_problems(llm=Ollama(model="llama3"), save_results="./data/results/benchmark_1_llama3.pickle")
-# invoke_decision_problems(llm=Ollama(model='bramvanroy/geitje-7b-ultra:Q3_K_M'),
-#                          save_results="./data/results/benchmark_1_geitje.pickle")
+# invoke_decision_problems(llm=Ollama(model="llama3"), save_results="./data/results/benchmark_1_llama3.pickle")
+invoke_decision_problems(llm=Ollama(model='bramvanroy/geitje-7b-ultra:Q3_K_M'),
+                         save_results="./data/results/benchmark_1_geitje.pickle")
